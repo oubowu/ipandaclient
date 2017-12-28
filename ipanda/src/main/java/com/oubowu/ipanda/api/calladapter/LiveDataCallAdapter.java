555c38dca5adfa47532accd3d5d0f20948080658
oubowu <wwwbryant@163.com>
2017-12-29 01:37:07 +0800

    注释

--- ipanda/src/main/java/com/oubowu/ipanda/api/calladapter/LiveDataCallAdapter.java
+++ ipanda/src/main/java/com/oubowu/ipanda/api/calladapter/LiveDataCallAdapter.java
@@ -16,7 +16,7 @@ import retrofit2.Response;
 /**
  * Created by Oubowu on 2017/12/27 11:48.
  */
-public class LiveDataCallAdapter<R> implements CallAdapter<R,LiveData<ApiResponse<R>>> {
+public class LiveDataCallAdapter<R> implements CallAdapter<R, LiveData<ApiResponse<R>>> {
 
     private final Type mResponseType;
 
@@ -31,14 +31,43 @@ public class LiveDataCallAdapter<R> implements CallAdapter<R,LiveData<ApiRespons
         return mResponseType;
     }
 
+
+   /* public <T> T create(final Class<T> service) {
+        Utils.validateServiceInterface(service);
+        if (validateEagerly) {
+            eagerlyValidateMethods(service);
+        }
+        return (T) Proxy.newProxyInstance(service.getClassLoader(), new Class<?>[] { service },
+                new InvocationHandler() {
+                    private final Platform platform = Platform.get();
+
+                    @Override public Object invoke(Object proxy, Method method, @Nullable Object[] args)
+                            throws Throwable {
+                        // If the method is a method from Object then defer to normal invocation.
+                        if (method.getDeclaringClass() == Object.class) {
+                            return method.invoke(this, args);
+                        }
+                        if (platform.isDefaultMethod(method)) {
+                            return platform.invokeDefaultMethod(method, service, proxy, args);
+                        }
+                        ServiceMethod<Object, Object> serviceMethod =
+                                (ServiceMethod<Object, Object>) loadServiceMethod(method);
+                        OkHttpCall<Object> okHttpCall = new OkHttpCall<>(serviceMethod, args);
+                        return serviceMethod.callAdapter.adapt(okHttpCall);
+                    }
+                });
+    }*/
+
+    // Retrofit.create最终调用这个方法请求数据
     @Override
     public LiveData<ApiResponse<R>> adapt(Call<R> call) {
         return new LiveData<ApiResponse<R>>() {
             AtomicBoolean started = new AtomicBoolean(false);
+
             @Override
             protected void onActive() {
                 super.onActive();
-                if (started.compareAndSet(false,true)){
+                if (started.compareAndSet(false, true)) {
                     call.enqueue(new Callback<R>() {
                         @Override
                         public void onResponse(@NonNull Call<R> call, @NonNull Response<R> response) {
